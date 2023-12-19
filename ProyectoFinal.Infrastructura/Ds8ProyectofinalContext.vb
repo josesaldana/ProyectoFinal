420380ea69f3b17500f49c14be4a30ca35a83a06
Jose Saldana <jmsaldana@gmail.com>
2023-12-19 18:19:24 -0500

    Re-order Homepage elements

--- ProyectoFinal.Infrastructura/Ds8ProyectofinalContext.vb
+++ ProyectoFinal.Infrastructura/Ds8ProyectofinalContext.vb
@@ -25,7 +25,6 @@ Partial Public Class Ds8ProyectofinalContext
     Public Overridable Property Usuarios As DbSet(Of Usuario)
 
     Protected Overrides Sub OnConfiguring(optionsBuilder As DbContextOptionsBuilder)
-        'TODO /!\ To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=ds8-proyectofinal;Integrated Security=SSPI;TrustServerCertificate=True")
     End Sub
 
